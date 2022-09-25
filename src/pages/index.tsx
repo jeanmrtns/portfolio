a6750ff65ab87e8965585e7d25ec2189ae05ef6f
Jean Martins <jeanmrtns4@gmail.com>
2022-09-25 08:11:56 -0300

    Setup config

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Jean&apos;s Portfolio</title>
+      </Head>
+
+      <h1>Hello beautiful world</h1>
+    </div>
+  )
+}
+
+export default Home
