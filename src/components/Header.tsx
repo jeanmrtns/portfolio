82c85ae89d90cf0914eb6ac7ce6b425c8d1c5f3f
Jean Martins <jeanmrtns4@gmail.com>
2022-09-25 08:51:15 -0300

    feat: add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,65 @@
+import { motion } from 'framer-motion'
+import { SocialIcon } from 'react-social-icons'
+
+const Header = () => {
+  return (
+    <motion.header className="flex p-5 items-start xl:items-center justify-between sticky top-0 max-w-7xl mx-auto z-20">
+      <motion.div
+        className="flex items-center"
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+      >
+        <SocialIcon
+          fgColor="gray"
+          bgColor="transparent"
+          url="https://github.com/jeanmrtns"
+        />
+        <SocialIcon
+          fgColor="gray"
+          bgColor="transparent"
+          url="https://instagram.com/jeanmrtns"
+        />
+        <SocialIcon
+          fgColor="gray"
+          bgColor="transparent"
+          url="https://linkedin.com/in/jeanmrtns"
+        />
+      </motion.div>
+
+      <motion.div
+        className="flex items-center gap-4 text-gray-300 cursor-pointer"
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+      >
+        <SocialIcon fgColor="gray" bgColor="transparent" network="email" />
+        <p className="uppercase hidden md:block text-sm text-gray-400">
+          Entre em contato
+        </p>
+      </motion.div>
+    </motion.header>
+  )
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +9,7 @@ const Home: NextPage = () => {
         <title>Jean&apos;s Portfolio</title>
       </Head>
 
-      <h1>Hello beautiful world</h1>
+      <Header />
     </div>
   )
 }
