19e083975fd94b8ca4a4ad4b2f89dbab881234c7
Jean Martins <jeanmrtns4@gmail.com>
2022-09-25 22:38:11 -0300

    feat: add metadata

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta
+          name="description"
+          content="Jean Martins - Desenvolvedor Frontend"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
